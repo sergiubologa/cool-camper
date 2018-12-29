8a81b61ca852a14dbf78caab3b71e1493295b1d8
Sergiu Bologa <sergiu.calin.bologa@gmail.com>
2018-12-29 23:03:18 +0200

    new FAQ texts added

--- src/static-data/faq-questions.js
+++ src/static-data/faq-questions.js
@@ -5,34 +5,25 @@ export default [
   },
   {
     question: "Ce e autorulota?",
-    answer: `Autorulota este un vehicul ce serveste atat ca mijloc de transport cat si ca spatiu de locuit, o casa pe roti, construita in jurul unui autosasiu.
-    Autorulotele  noastre sunt echipate cu 5 locuri pe scaune, 5 locuri de dormit, bucatarie (dotata cu aragaz, frigider , congelator la anumite modele, chiuveta, blat de lucru), baie (cabina de dus cu apa calda/rece, WC, chiuveta), spatii mari de depozitare, apa calda si caldura, sistem de conectare la retea electrica, aer conditionat, spatiu depozitare si transport biciclete, TV LCD, espressor de cafea.
-    Panouri solare 140 W
-    Cablu de alimentare 220W
-    Rezervor de apa reziduala (100l)
-    Rezervor apa curata (120l)
-    Veranda Fiamma extensibila (3m)
-    Albumul foto cu autorulotele CoolCamper poate fi vizualizat aici.
-    Toate autorulotele noastre sunt echipate de iarna, cu incalzire pe gaz sau motorina, protectie antiinghet a instalatiilor, fapt pentru care pot fi utilizate si in conditii de confort deosebit, la temperaturi intre -40 + 50 grade Celsius.
-    Autorulotele sunt pe deplin functionale atat in modul camping (conectate la surse de utilitati, in camping) , cat si in modul off camping (pe plaja sau in varf de munte, fara curent electric sau apa/gaz).`
+    answer: `Autorulota este un vehicul ce serveste atat ca mijloc de transport cat si ca spatiu de locuit, o casa pe roti, construita in jurul unui autosasiu.`
   },
   {
-    question: `Vreau sa fac un circuit european, insa nu stiu de unde sa o iau.
-      Ma puteti ajuta cu informatii?`,
-    answer: `Categoric. Incercam sa fim mai mult decat un serviciu de inchirieri de autorulote. Vrem sa fim prietenul tau de nadejede pe toata perioada calatoriei cu autorulota si de ce nu, poate chiar si dupa :)
-      De aceea pe langa autorulota noi iti vom sugera diferite itinerarii.
-       Odata decisa perioada inchirierii si ruta pe care vrei sa o faci alaturi de cei prieteni sau de familie, noi iti vom trimite pe email toate detaliile calatoriei tale. 
-      Vei avea timp suficient sa studiezi ceea ce noi iti propunem si daca consideri ca vrei sa modifici traseul, te putem ajuta.
-      Ne poti contacta cu incredere pe e-mail bianca@coolcamper.ro sau telefon 0742291018. Iti vom da toate detaliile de care ai nevoie.
-      `
+    question: "Care sunt specificatiile tehnice ale autorulotelor CoolCamper?",
+    answer: `Autorulotele  noastre sunt echipate cu 5 locuri pe scaune, 5 locuri de dormit, bucatarie (dotata cu aragaz, frigider, congelator la anumite modele, chiuveta, blat de lucru), baie (cabina de dus cu apa calda/rece, WC, chiuveta), spatii mari de depozitare, apa calda si caldura, sistem de conectare la retea electrica, aer conditionat, spatiu depozitare si transport biciclete, TV LCD, espressor de cafea. De asemenea sunt echipate cu panouri solare 140 W + Cablu de alimentare 220W + Rezervor de apa reziduala (100l) + Rezervor apa curata (120l) + Veranda Fiamma extensibila (3m).`
+  },
+  {
+    question: "Sunt autorulotele CoolCamper echipate adecvat pentru iarna?",
+    answer: `Toate autorulotele noastre sunt echipate de iarna, cu incalzire pe gaz sau motorina, protectie antiinghet a instalatiilor, fapt pentru care pot fi utilizate si in conditii de confort deosebit, la temperaturi intre -40 + 50 grade Celsius.
+    Autorulotele sunt pe deplin functionale atat in modul camping (conectate la surse de utilitati, in camping), cat si in modul off camping (pe plaja sau in varf de munte, fara curent electric sau apa/gaz). `
+  },
+  {
+    question: "Unde pot sa vad poze cu autorulotele CoolCamper?",
+    answer: `Albumul foto cu autorulotele CoolCamper poate fi vizualizat aici.`
   },
   {
     question: "Cati pasageri poate gazdui o autorulota?",
     answer: `Capacitatea autorulotelor noastre este de 5locuri pe scaune, 5 locuri de dormit.
-    Paturile sunt localizate astfel:
-    - pat dublu deasupra cabinei,
-    - pat dublu in spate,
-    - pat single suprapus patului dublu din spate.
+    Paturile sunt localizate astfel: pat dublu deasupra cabinei, pat dublu in spate si pat single suprapus patului dublu din spate.
     `
   },
   {
@@ -41,49 +32,22 @@ export default [
       "Autorulota consuma motorina, aproximativ 9-11 l/100 km, in functie de incarcare si stilul de condus."
   },
   {
-    question: "Exista taxe de drum speciale in afara Romaniei?",
-    answer: `Autorulotele sunt considerate autoturisme speciale tip M1. In majoritatea tarilor au aceleasi costuri (vigneta) ca si autoturismele. Exceptii fac tarile cu taxare de autostrada, unde tarifele pot fi cu 30% mai mari decat ale autoturismelor.`
-  },
-
-  {
     question: "Care este viteza de deplasare cu autorulota?",
     answer: `In cazul autorulotelor, viteza de deplasare este cu aprox. 30% mai scazuta decat cea a autoturismelor. Conform legii autorulotele au aceleasi restrictii ca si autoturismele, nu exista nici o limitare speciala.
-    Pe autostrazi viteza de deplasare recomandata este de 100-110 km/h
-    Pe sosele nationale 80 km/h
-    Pe drumuri in curbe va recomandam atentie sporita, datorita greutatii si a inaltimii poate produce balans. In general pe aceste tip de drumuri recomandam o viteza cu 10 km/h mai mica decat indicatorul de viteza de pe drum.`
+    Pe autostrazi viteza de deplasare recomandata este de 100-110 km/h iar pe sosele nationale 80 km/h.
+    `
+  },
+  {
+    question: "Cum trebuie sa conduc pe drumuri inguste sau in curba?",
+    answer: `Pe drumuri in curbe iti recomandam atentie sporita, datorita greutatii si a inaltimii poate produce balans. In general pe aceste tip de drumuri recomandam o viteza cu 10 km/h mai mica decat indicatorul de viteza de pe drum.
+    Pe drumurile inguste iti recomandam sa conduci incet, astfel incat sa poti sa opresti rapid daca apare un autovehciul din sensul opus.
+    `
   },
   {
     question: "Putem conduce mai multe persoane autorulota?",
     answer: `Da, autorulota poate fi condusa de orice persoana cu varsta minima 28 ani care detine permis de conducere cat. B de minim 5 ani (nu este o lege din Codul Rutier ci doar o cerinta CoolCamper).
     Nu limitam numarul de soferi per inchiriere.
     Totusi trebuie sa te asiguri ca persoana care va conduce autorulota este suficient de responsabila si experimentata, fiind vorba de un vehicul de mari dimensiuni.`
-  },
-  {
-    question: "Unde pot parca autorulota?",
-    answer: `Oriunde, atat timp cat este un loc de parcare legal. Avand regim de autoturism poti parca oriunde.
-    Insa, daca, de exemplu, vrei sa innoptati pe autostrada, trebuie sa tii cont de urmatoarele reguli:
-    - nu aveti voie sa deschideti veranda sau sa scoateti afara masa si scaunele de camping. In acest caz se considera campare in loc neamenajat si veti fi amendati de catre politia din tara respectiva.
-    - in anumite tari din Europa nu aveti voie sa consumati alcool in autorulota daca sunteti in parcare si daca perdelele nu sunt inchise; se poate considera consum de alcool la volan si puteti fi amendati.
-    - in majoritatea benzinariilor din afara tarii sunt locuri special amenajate pt autorulote , care permit alimentarea cu apa, deversarea apelor uzate si golirea casetei de toaleta, in mod gratuit; in Romania aceste facilitati inca nu sunt prezente si se obisnuieste alimentarea cu apa in benzinarii, golirea apelor uzate in reteaua de canalizare.`
-  },
-  {
-    question:
-      "Am o problema, o defectiune sau nu stiu sa folosesc anumite dotarii ale autorulotei. Ce fac?",
-    answer: `In cazul defectiunilor pot aparea, rareori urmatoarele cazuri:
-    Defectiuni ale autovehiculului
-    Defectiuni ale sistemelor interioare/zonei de locuit
-    In cazul defectiunilor autovehiculului:
-    Avand modele noi de autorulote (2019) este putin probabil sa aveti probleme. Totusi, in cazul unei defectiuni, te rugam sa ne contactezi pe whatsapp sau telefonic cu descrierea problemei.
-    Daca autorulota nu poate fi deplasata, se te rugam sa apelezi nr. de asistenta rutiera europeana Fiat Camper Assistance (tel: 00800 34281111* sau alternativ 0039 024441 2160). Interventiile (remorcare, reparatie sub garantie europeana) sunt gratuite.
-    In caz de defectiuni ale spatiului de locuit/sistemelor interioare:
-    Consulta manualul de bord. Intre timp contacteaza-ne pe whatsapp sau telefonic si noi o sa iti raspundem indiferent de ora. De cele mai multe ori putem identifica si rezolva problema impreuna in mai putin de 2 minute.`
-  },
-  {
-    question: "Am avut un accident. Ce trebuie sa fac?",
-    answer: `Asigura-te ca toti pasagerii sunt teferi si intr-un loc sigur.
-    Informeaza-ne imediat, telefonic sau pe whatsapp, care sunt pagubele si daca autorulota este deplasabila.
-    Obtine un raport de constatare de la organele abilitate.
-    Avem asigurare full Casco (cu fransiza de 250 Eur/primul eveniment; 500 Eur/al doilea eveniment).`
   },
   {
     question: "Aveti limita de km?",
@@ -97,24 +61,38 @@ export default [
     Din pacate pentru tarile care nu sunt acoperite de RCA si Casco aveti obligativitatea sa incheiati o  polita separata la intrarea in tara respectiva.`
   },
   {
-    question:
-      "Sunt plecat cu autorulota si vreau sa prelungesc perioada inchirierii. E posibil?",
-    answer: `Din pacate, in majoritatea cazurilor nu, dar cu toate acestea nu ezita sa ne suni pentru a verifica disponibilitatea autorulotei.
+    question: "Unde pot parca autorulota?",
+    answer: `Oriunde. Avand regim de autoturism poti parca oriunde.`
+  },
+  {
+    question: "Pot sa inoptez pe autostrada?",
+    answer: `Da dar trebuie sa tine cont ca nu aveti voie sa deschideti veranda sau sa scoateti afara masa si scaunele de camping. In acest caz se considera campare in loc neamenajat si veti fi amendati de catre politia din tara respectiva.
+    De asemenea, in anumite tari din Europa nu aveti voie sa consumati alcool in autorulota daca sunteti in parcare si daca perdelele nu sunt inchise; se poate considera consum de alcool la volan si puteti fi amendati.
     `
   },
   {
-    question: "Care este garantia perceputa?",
-    answer: `Valoarea garantiei este de ..................  Garantia se va incasa doar prin card de credit/debit emis pe numele titularului de contract sau prin virament bancar efectuat de pe numele titularului de contract.
-    Garantia se va percepe cu minim 10 zile inainte de preluarea autovehiculului, prin link securizat de plata/virament, si se va returna in termen de 72 ore dupa returnarea in aceleasi conditii ale autovehiculului.
-    Garantia nu se va debita, ci se va bloca intr-un cont intermediar pe toata durata inchirierii.
-    CoolCamper nu solicita si nu stocheaza informatii cu privire la datele de identificare ale titularului cardului de debit/credit, acestea vor fi procesate de catre un procesator autorizat (mobilpay).
-    Nu se accepta garantii in numerar????????`
+    question: "De unde pot alimenta cu apa autorulota?",
+    answer: `In majoritatea benzinariilor din afara tarii sunt locuri special amenajate pt autorulote , care permit alimentarea cu apa, deversarea apelor uzate si golirea casetei de toaleta, in mod gratuit.
+    In Romania aceste facilitati inca nu sunt prezente si se obisnuieste alimentarea cu apa in benzinarii, golirea apelor uzate in reteaua de canalizare.
+    `
   },
   {
-    question: "As dori sa vad autorulotele inainte de rezervare. Se poate?",
-    answer: `Bineinteles! Te incurajam sa vii sa ne vizitezi, astfel iti vei putea face o imagine clara a ceea ce inseamna o autorulota. 	
-    
-    Pentru a programa o vizita la noi te rugam sa ne contactezi prin email la bianca@coolcamper.ro  sau la nr de telefon 0742291018.`
+    question: "Am o defectiune a autorulotei. Ce fac?",
+    answer: `Avand modele noi de autorulote (2019) este improbabil sa se defecteze. Totusi, in cazul unei defectiuni, te rugam sa ne contactezi pe whatsapp sau telefonic cu descrierea problemei.
+    Daca autorulota nu poate fi deplasata, se te rugam sa apelezi nr. de asistenta rutiera europeana Fiat Camper Assistance (tel: 00800 34281111* sau alternativ 0039 024441 2160). Interventiile (remorcare, reparatie sub garantie europeana) sunt gratuite.
+    `
+  },
+  {
+    question: "Ce fac in caz de defectiuni al spatiului de locuit?",
+    answer: `In caz de defectiuni ale spatiului de locuit/sistemelor interioare iti recomandam sa consulti manualul de bord. Intre timp contacteaza-ne pe whatsapp sau telefonic si noi o sa iti raspundem indiferent de ora. De cele mai multe ori putem identifica si rezolva problema impreuna.
+    `
+  },
+  {
+    question: "Am avut un accident. Ce trebuie sa fac?",
+    answer: `Asigura-te ca toti pasagerii sunt teferi si intr-un loc sigur.
+    Informeaza-ne imediat, telefonic sau pe whatsapp, care sunt pagubele si daca autorulota este deplasabila.
+    Obtine un raport de constatare de la organele abilitate.
+    Avem asigurare full Casco (cu fransiza de 250 Eur/primul eveniment; 500 Eur/al doilea eveniment).`
   },
   {
     question:
@@ -128,6 +106,11 @@ export default [
     `
   },
   {
+    question: "Cum platesc avansul, in cazul unei rezervari?",
+    answer: `Plata se va putea efectua prin virament bancar sau online, cu cardul, prin link securizat. Din pacate nu acceptam plata in numerar.
+    `
+  },
+  {
     question: "Cu cat timp inainte trebuie sa rezerv?",
     answer: `Iti recomandam sa ne contactezi cat mai repede, dupa ce te-ai decis ca vrei sa pornesti in aceasta aventura. 
     Nu avem o regula a rezervarilor, dar primul venit e primul servit.
@@ -136,17 +119,47 @@ export default [
   {
     question: "Am rezervat si se apropie data plecarii. Ce trebuie sa fac?",
     answer: `Ora preluarii: zilnic incepand cu ora 10.00 (pentru ore diferite sau aranjamente speciale te rugam sa ne contactezi in avans)
+    Ora predarii: zilnic pana in ora 20.00 (te rugam sa ne suni cu aprox. 2 ore inainte de predare).
+    `
+  },
+  {
+    question: "Care este garantia perceputa?",
+    answer: `Valoarea garantiei este de …………………....  Garantia se va incasa doar prin card de credit/debit emis pe numele titularului de contract sau prin virament bancar efectuat de pe numele titularului de contract.
+    `
+  },
+  {
+    question: "Cum trebuie procedat pentru a plati garantia?",
+    answer: `Garantia se va percepe cu minim 10 zile inainte de preluarea autovehiculului, printr-un link securizat de plata/virament, si se va returna in termen de 72 ore dupa returnarea in aceleasi conditii ale autovehiculului.
+    Garantia nu se va debita, ci se va bloca intr-un cont intermediar pe toata durata inchirierii. Noi nu stocam informatile cu privire la datele de identificare ale titularului cardului de debit/credit, acestea vor fi procesate de catre un procesator autorizat (...............).
+    Nu acceptam garantii in numerar.
+    `
+  },
+  {
+    question: "Cu ce sunt echipate autorulotele?",
+    answer: `Autorulotele noastre vin echipate cu suport de biciclete (2-4 biciclete), veranda retractabila (marchiza), set de camping (masa + scaune de exterior), un set de consumabile (gaz - 1 butelie conectata, una de rezerva, subst chimice pt toaleta ecologica, hartie igienica solubila in apa). Veti avea la dispozitie si un set de bucatarie (vesela de unica folosinta, etc) si lenjerii de pat, inclusiv perne si paturi.
+    `
+  },
+  {
+    question: "Exista taxe de drum speciale in afara Romaniei?",
+    answer: `Autorulotele sunt considerate autoturisme speciale tip M1. In majoritatea tarilor au aceleasi costuri (vigneta) ca si autoturismele. Exceptii fac tarile cu taxare de autostrada, unde tarifele pot fi cu 30% mai mari decat ale autoturismelor.`
+  },
+  {
+    question:
+      "Sunt plecat cu autorulota si vreau sa prelungesc perioada inchirierii. E posibil?",
+    answer: `Din pacate, in majoritatea cazurilor nu, dar cu toate acestea nu ezita sa ne suni pentru a verifica disponibilitatea autorulotei.
+    `
+  },
+  {
+    question: "As dori sa vad autorulotele inainte de rezervare. Se poate?",
+    answer: `Bineinteles! Te incurajam sa vii sa ne vizitezi, astfel iti vei putea face o imagine clara a ceea ce inseamna o autorulota. 	
     
-    Ora predarii: zilnic pana in ora 20.00 (te rugam sa ne suni cu aprox. 2 ore inainte de predare)
-    
-    Autorulotele noastre vin echipate cu:
-    - suport de biciclete (2-4 biciclete);
-    - veranda retractabila (marchiza);
-    - set de camping (masa + 4-7 scaune de exterior);
-    - un set de consumabile (gaz - 1 butelie conectata, una de rezerva; hartie igienica solubila in apa, etc.)
-    - lenjerii de pat, inclusiv perne si paturi (daca este cazul);
-    - set de bucatarie (vesela de unica folosinta, etc)  - datorita miscarii produc zgomot in mers si din acest motiv ceea ce iti punem la dispozitie e doar vesela de baza
-    - prosoape si obiecte de igiena personala.
+    Pentru a programa o vizita la noi te rugam sa ne contactezi prin email la bianca@coolcamper.ro  sau la nr de telefon 0742291018.`
+  },
+  {
+    question:
+      "Vreau sa fac un circuit european, insa nu stiu de unde sa o iau. Ma puteti ajuta cu informatii?",
+    answer: ` Da. Odata decisa perioada inchirierii si ruta pe care vrei sa o faci, iti vom trimite pe email un itinerariu cu toate detaliile calatoriei tale (distanta parcursa pe zi, nr de ore conduse, puncte de oprire, locuri de campare, atractii turistice, etc.).
+    Studiaza itinerariul propus, iar mai apoi te putem ajuta sa-l modifici conform planurilor tale, daca va fi cazul.
     `
   }
 ];
