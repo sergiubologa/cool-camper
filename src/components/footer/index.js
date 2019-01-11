b134c353209541d0a147fab1a4dcc1f6feb3ad79
Sergiu Bologa <sergiu.bologa@uipath.com>
2019-01-11 09:54:20 +0200

    Edit footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -10,19 +10,15 @@ export default () => (
             CoolCamper
           </Link>
           <small>
-            Site-ul nostru foloseste cookies pentru a imbunatati experienta de
-            navigare. Continuarea navigarii implica acceptarea{" "}
-            <Link to="politica-de-confidentialitate" className="footer__link">
-              Politicii de Confidentialitate
-            </Link>{" "}
-            a CoolCamper.ro.
-          </small>{" "}
-          <br />
-          <small>Toate drepturile rezervate CoolCamper.ro ©2018</small>
+            CoolCamper este un serviciu de{" "}
+            <strong>închiriere autorulote</strong> cu care poți să călătorești
+            oriunde în Europa, în locuri în care nu poți să ajungi cu avionul.
+            Be cool, be a CoolCamper!
+          </small>
         </div>
 
         <div className="footer__data">
-          <div className="footer__data__item">
+          <div className="footer__data__item footer__pages">
             <div className="footer__row">
               <Link to="contact" className="footer__link">
                 Contact
@@ -39,11 +35,10 @@ export default () => (
               </Link>
             </div>
           </div>
-          <div className="footer__data__item">Lorem ipsum</div>
-          <div className="footer__data__item">
+          <div className="footer__data__item footer__social">
             <div className="footer__row">
               <a
-                href="https://www.facebook.com"
+                href="https://www.facebook.com/CoolCamperCluj"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="footer__link"
@@ -63,13 +58,31 @@ export default () => (
             </div>
             <div className="footer__row">
               <a
-                href="https://twitter.com"
+                href="https://twitter.com/CoolCamperCluj"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="footer__link"
               >
                 Twitter
               </a>
+            </div>
+          </div>
+          <div className="footer__data__item footer__copyright">
+            <div className="footer__row">
+              <small>
+                Site-ul nostru foloseste cookies pentru a imbunatati experienta
+                de navigare. Continuarea navigarii implica acceptarea{" "}
+                <Link
+                  to="politica-de-confidentialitate"
+                  className="footer__link"
+                >
+                  Politicii de Confidentialitate
+                </Link>{" "}
+                a CoolCamper.ro.
+              </small>{" "}
+              <br />
+              <br />
+              <small>Toate drepturile rezervate CoolCamper.ro ©2018</small>
             </div>
           </div>
         </div>
