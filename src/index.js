46b7c257424ec06c9a3c657fd7517cf204e0f152
Sergiu Bologa <sergiu.calin.bologa@gmail.com>
2019-01-13 22:21:43 +0200

    Add google bot on error

--- src/index.js
+++ src/index.js
@@ -36,6 +36,32 @@ if (process.env.NODE_ENV !== "production") {
 // Run polyfills
 smoothscroll.polyfill();
 
+window.onerror = function(message, url, lineNo, colNo, error) {
+  console.log(arguments);
+
+  let container = document.createElement("div");
+
+  container.style.color = "red";
+  container.style.position = "fixed";
+  container.style.background = "#eee";
+  container.style.padding = "2em";
+  container.style.top = "1em";
+  container.style.left = "1em";
+
+  let msg = document.createElement("pre");
+  msg.innerText = [
+    "Message: " + message,
+    "URL: " + url,
+    "Line: " + lineNo,
+    "Column: " + colNo,
+    "Stack: " + (error && error.stack)
+  ].join("\n");
+
+  container.appendChild(msg);
+
+  document.body.appendChild(container);
+};
+
 ReactDOM.render(
   <Router>
     <ScrollToTop>
