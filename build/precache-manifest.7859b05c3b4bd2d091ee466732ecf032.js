5cc0af3e952c303805e6c4cf5520eaa671bb3741
Sergiu Bologa <sergiu.calin.bologa@gmail.com>
2020-05-17 01:39:03 +0300

    build

---
+++ build/precache-manifest.7859b05c3b4bd2d091ee466732ecf032.js
@@ -0,0 +1,378 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af85bdb0728d8d6f4c443b45c43590e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3b4dc608497f35e573f",
+    "url": "/static/css/10.4d4d1c08.chunk.css"
+  },
+  {
+    "revision": "0ec76f148d9a15e320a7",
+    "url": "/static/css/4.764ccc25.chunk.css"
+  },
+  {
+    "revision": "3a67e92b6c40cb5aec4d",
+    "url": "/static/css/5.3bc790af.chunk.css"
+  },
+  {
+    "revision": "80d1fbffeef8184e2a04",
+    "url": "/static/css/6.3936a145.chunk.css"
+  },
+  {
+    "revision": "1d157d0edef9daace9e3",
+    "url": "/static/css/7.18585a14.chunk.css"
+  },
+  {
+    "revision": "54a80e4510cef1271209",
+    "url": "/static/css/8.3ffedea6.chunk.css"
+  },
+  {
+    "revision": "ddf6ec6f3fecf0d65607",
+    "url": "/static/css/9.8ada02ad.chunk.css"
+  },
+  {
+    "revision": "3de041402116f0abf392",
+    "url": "/static/css/main.bd82263d.chunk.css"
+  },
+  {
+    "revision": "180a12f0653d7ef3c0ad",
+    "url": "/static/js/0.fff5e31d.chunk.js"
+  },
+  {
+    "revision": "751a3545d600f7aa2fc0582f49926f51",
+    "url": "/static/js/0.fff5e31d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d57ea60b40f6d4849a8",
+    "url": "/static/js/1.9f40e14d.chunk.js"
+  },
+  {
+    "revision": "e3b4dc608497f35e573f",
+    "url": "/static/js/10.75a6f80b.chunk.js"
+  },
+  {
+    "revision": "613f667c408c5fedce8f",
+    "url": "/static/js/11.ef9da0f4.chunk.js"
+  },
+  {
+    "revision": "ade1830e359e12b913ed",
+    "url": "/static/js/12.ffaffdb1.chunk.js"
+  },
+  {
+    "revision": "765ccf18b7c8b6d2d9ea",
+    "url": "/static/js/13.61ef6406.chunk.js"
+  },
+  {
+    "revision": "90e2a32945c6c14086a5",
+    "url": "/static/js/14.47e60c95.chunk.js"
+  },
+  {
+    "revision": "a4795f7f025d2a138441",
+    "url": "/static/js/15.8f84d76a.chunk.js"
+  },
+  {
+    "revision": "0ec76f148d9a15e320a7",
+    "url": "/static/js/4.b9b9f02d.chunk.js"
+  },
+  {
+    "revision": "dd30ad2c00174e59bb32191ea757f584",
+    "url": "/static/js/4.b9b9f02d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a67e92b6c40cb5aec4d",
+    "url": "/static/js/5.2fd3132b.chunk.js"
+  },
+  {
+    "revision": "5deab6ac78b8330902786be1f5dede27",
+    "url": "/static/js/5.2fd3132b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80d1fbffeef8184e2a04",
+    "url": "/static/js/6.f9704da5.chunk.js"
+  },
+  {
+    "revision": "1d157d0edef9daace9e3",
+    "url": "/static/js/7.f8283429.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/static/js/7.f8283429.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54a80e4510cef1271209",
+    "url": "/static/js/8.6c5280f5.chunk.js"
+  },
+  {
+    "revision": "ddf6ec6f3fecf0d65607",
+    "url": "/static/js/9.5c8e7c48.chunk.js"
+  },
+  {
+    "revision": "3de041402116f0abf392",
+    "url": "/static/js/main.2e0a26e5.chunk.js"
+  },
+  {
+    "revision": "8301f7738b3189b4d372",
+    "url": "/static/js/runtime-main.655f5ba5.js"
+  },
+  {
+    "revision": "6695af9986412e75985538255ca87866",
+    "url": "/static/media/404.6695af99.svg"
+  },
+  {
+    "revision": "5ddf205265cd6e7dbadd9cfdb5acc81f",
+    "url": "/static/media/Alesund_Norway_1.5ddf2052.jpg"
+  },
+  {
+    "revision": "6ef73355ed8fc9e067950f067ddeb664",
+    "url": "/static/media/Atlantic_Road_Bergen_Norway_2.6ef73355.jpg"
+  },
+  {
+    "revision": "1e558eca67135800eef50f9633f852ea",
+    "url": "/static/media/Atlantic_Road_Bergen_Norway_4.1e558eca.jpg"
+  },
+  {
+    "revision": "5d53c8c08123cd309d570360d9a70bed",
+    "url": "/static/media/Copenhaga_Denmark_2.5d53c8c0.jpg"
+  },
+  {
+    "revision": "ef98fb734019d3d03c3b7cf0063520de",
+    "url": "/static/media/Dubrovnik_Crotia_2.ef98fb73.jpg"
+  },
+  {
+    "revision": "880a5557760503cb3d91a0f7e18dbd82",
+    "url": "/static/media/Dubrovnik_Crotia_3.880a5557.jpg"
+  },
+  {
+    "revision": "6ae568f2293006a858d1f46ac72c1893",
+    "url": "/static/media/Dubrovnik_Crotia_5.6ae568f2.jpg"
+  },
+  {
+    "revision": "0e8916c4759c6c09f8aa7f91f99f15ab",
+    "url": "/static/media/Dubrovnik_Crotia_6.0e8916c4.jpg"
+  },
+  {
+    "revision": "45bd47a5169dc3facb17bed348e6ca64",
+    "url": "/static/media/Geirengerfjord_Norway_2.45bd47a5.jpg"
+  },
+  {
+    "revision": "c1a686f781f331b6a0b9bea094496ae2",
+    "url": "/static/media/Italy_mountains.c1a686f7.jpg"
+  },
+  {
+    "revision": "fccc5a53fec863b64614ba27fa28694a",
+    "url": "/static/media/Italy_scooter.fccc5a53.jpg"
+  },
+  {
+    "revision": "1b3ec5b04917acc21ec74d83262b624a",
+    "url": "/static/media/Kotor_Muntenegru_1.1b3ec5b0.jpg"
+  },
+  {
+    "revision": "986514ff587a4c4a73b8b84adcb7564e",
+    "url": "/static/media/Kotor_Muntenegru_5.986514ff.jpg"
+  },
+  {
+    "revision": "9400201061241674df5db42e82806e19",
+    "url": "/static/media/PlitviceLakes_Croatia_1.94002010.jpg"
+  },
+  {
+    "revision": "6f21d9cb76866e49974c53fe1c95a358",
+    "url": "/static/media/PlitviceLakes_Croatia_2.6f21d9cb.jpg"
+  },
+  {
+    "revision": "4c86c6bb5a09ffca96c50489f7cb93ec",
+    "url": "/static/media/PlitviceLakes_Croatia_5.4c86c6bb.jpg"
+  },
+  {
+    "revision": "7e8847ddf4668909bc2a623354a1bd3a",
+    "url": "/static/media/Skansen_Sweden_4.7e8847dd.jpg"
+  },
+  {
+    "revision": "4ce0c1e342f2566e0deb397e103d9e82",
+    "url": "/static/media/Split_Croatia_1.4ce0c1e3.jpg"
+  },
+  {
+    "revision": "1019d3906b1e9f22b31a07ea6641f8de",
+    "url": "/static/media/Stockholm_Sweden_6.1019d390.jpg"
+  },
+  {
+    "revision": "b2ea1889360ab98cf5bbdaca99c4ec59",
+    "url": "/static/media/Stockholm_Sweden_7.b2ea1889.jpg"
+  },
+  {
+    "revision": "e2634e96c9ad4694d5133cc83e2c6564",
+    "url": "/static/media/at.e2634e96.svg"
+  },
+  {
+    "revision": "71010dff44cc2c8dfb46906c7add051f",
+    "url": "/static/media/ba.71010dff.svg"
+  },
+  {
+    "revision": "805a7087edb86646755717d77a6c14a9",
+    "url": "/static/media/bathroom.805a7087.jpeg"
+  },
+  {
+    "revision": "376d8a364ab3cc839b8a2a3d39afea6f",
+    "url": "/static/media/bedroom_1.376d8a36.jpg"
+  },
+  {
+    "revision": "635ddc36d74a3d44dbd2918197866dbe",
+    "url": "/static/media/bikes.635ddc36.jpeg"
+  },
+  {
+    "revision": "af46064c64232a69dfaf56597dce7f50",
+    "url": "/static/media/budapest_1.af46064c.jpg"
+  },
+  {
+    "revision": "eb1416e02baeee91a39f721e871caf23",
+    "url": "/static/media/dk.eb1416e0.svg"
+  },
+  {
+    "revision": "23f294f5333bab8ed205abef285955e7",
+    "url": "/static/media/ford.23f294f5.svg"
+  },
+  {
+    "revision": "b1156355de9691d768df19a8a2b44da4",
+    "url": "/static/media/fr.b1156355.svg"
+  },
+  {
+    "revision": "c32552a8a4a6fed9e3fbf24948985dd3",
+    "url": "/static/media/gift.c32552a8.svg"
+  },
+  {
+    "revision": "76c530e392e94c88e9e3008d155edd4a",
+    "url": "/static/media/hallstatt-3609863_1920.76c530e3.jpg"
+  },
+  {
+    "revision": "4680d6323b39f2d7bd88116f757d8838",
+    "url": "/static/media/hr.4680d632.svg"
+  },
+  {
+    "revision": "0d7409f88bca8325938e46e3ef672716",
+    "url": "/static/media/hu.0d7409f8.svg"
+  },
+  {
+    "revision": "1d2d602444f1762eb1e0c4bd3f607ac5",
+    "url": "/static/media/inbox.1d2d6024.svg"
+  },
+  {
+    "revision": "a0bb15bb4050d036925525e6c4816edd",
+    "url": "/static/media/inside_living.a0bb15bb.jpeg"
+  },
+  {
+    "revision": "fef8fb8ead425f48fc61e0f4ee0f0f83",
+    "url": "/static/media/inside_seats.fef8fb8e.jpeg"
+  },
+  {
+    "revision": "682d37515cbd367be4c941b5681857fb",
+    "url": "/static/media/inside_view_back.682d3751.jpg"
+  },
+  {
+    "revision": "5d09753b6ca5e9b1cb3c8171c2a80d85",
+    "url": "/static/media/inside_view_back_2.5d09753b.jpg"
+  },
+  {
+    "revision": "381c2f17389d1ec475484a122d9f4640",
+    "url": "/static/media/inside_view_front.381c2f17.jpg"
+  },
+  {
+    "revision": "8d15de04f5f6e8e89cab4e5eb237f607",
+    "url": "/static/media/it.8d15de04.svg"
+  },
+  {
+    "revision": "9b7ed44218bcf4060d28e9d1c1200132",
+    "url": "/static/media/kitchen.9b7ed442.jpeg"
+  },
+  {
+    "revision": "1f1b462044a82eb7f248d23ae57d2bd7",
+    "url": "/static/media/map_light.1f1b4620.svg"
+  },
+  {
+    "revision": "da1dbd957c21cb4ad02683e93a5ddd90",
+    "url": "/static/media/mobile-phone.da1dbd95.svg"
+  },
+  {
+    "revision": "74cc987acd9c1650e738c7d5fd69027b",
+    "url": "/static/media/motorhome-background-big.74cc987a.jpg"
+  },
+  {
+    "revision": "fefa5aa9a32bfe3e47570d806a52ea21",
+    "url": "/static/media/motorhome-background-small.fefa5aa9.jpg"
+  },
+  {
+    "revision": "9c36d4bbffa563d817ff574aed8f41a2",
+    "url": "/static/media/motorhome.9c36d4bb.svg"
+  },
+  {
+    "revision": "0b41df77e951a30bbfccfd0a3714a1a3",
+    "url": "/static/media/no.0b41df77.svg"
+  },
+  {
+    "revision": "6fe704c2677e7177e7e448d69a7a3b24",
+    "url": "/static/media/outside_1.6fe704c2.jpeg"
+  },
+  {
+    "revision": "7de7cacebcbb1ae7fa87ce58fd06fbea",
+    "url": "/static/media/outside_2.7de7cace.jpeg"
+  },
+  {
+    "revision": "aec8e27c433c5d2b6110c1e937970bda",
+    "url": "/static/media/paris_1.aec8e27c.jpg"
+  },
+  {
+    "revision": "16a6b77f1b672bd71e10899748ad07dd",
+    "url": "/static/media/paris_3.16a6b77f.jpg"
+  },
+  {
+    "revision": "572302a3f389d3e70623f268bf123f1b",
+    "url": "/static/media/rs.572302a3.svg"
+  },
+  {
+    "revision": "84d6d76a15dd8f2e02e8144b41cdc728",
+    "url": "/static/media/salzburg_1.84d6d76a.jpg"
+  },
+  {
+    "revision": "fe725901338e5651e1429ef0b241538a",
+    "url": "/static/media/se.fe725901.svg"
+  },
+  {
+    "revision": "194939399bc0f3bfb15d47288c3c2855",
+    "url": "/static/media/secure-data.19493939.svg"
+  },
+  {
+    "revision": "0f313e2c3f9aa597e08771eeae55d98a",
+    "url": "/static/media/shower.0f313e2c.jpeg"
+  },
+  {
+    "revision": "ffe9e7a4c866d11a8a3aada98520c568",
+    "url": "/static/media/si.ffe9e7a4.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "735a186ac41204b69e5523895d5afd4b",
+    "url": "/static/media/strasbourg.735a186a.jpg"
+  },
+  {
+    "revision": "13dabf4bc5d74fa72d1f210b5169db0c",
+    "url": "/static/media/super-thank-you.13dabf4b.svg"
+  },
+  {
+    "revision": "37b729dd28df72baac48efe219a0d2dc",
+    "url": "/static/media/support.37b729dd.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1ad533deeb641ea058dee170d2f56d6a.js"
+  "/precache-manifest.7859b05c3b4bd2d091ee466732ecf032.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
